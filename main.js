0d2fc1d6e409c9ac527c26bbb2e79e7a1cbe7239
Thomas-Spb <thomas-spb@list.ru>
2024-02-29 16:03:22 +0300

    Modal const

---
+++ main.js
@@ -0,0 +1,10 @@
+'use sctrict';
+
+const modalTitle = document.querySelector('.modal__title');
+const modalForm = document.querySelector('.modal__form');
+const modalCheckbox = document.querySelector('.modal__checkbox');
+const modalInputDiscount = document.querySelector('.modal__input_discount');
+console.log('modalTitle: ', modalTitle);
+console.log('modalForm: ', modalForm);
+console.log('modalCheckbox: ', modalCheckbox);
+console.log('modalInputDiscount: ', modalInputDiscount);
